f5b354ed4a484f034d953004a5194e659b89c641
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-03-20 18:24:42 +0800

    feat: add responsive design

--- src/components/days.tsx
+++ src/components/days.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   dayKeysMap,
   isDateInMonth,
@@ -7,6 +9,7 @@ import {
 import dayjs from "dayjs";
 import Event from "@/components/event";
 import { cn } from "@/lib/utils";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 interface DaysProps {
   date: DateDetail;
@@ -17,13 +20,19 @@ export default function Days({ date, viewingDate }: DaysProps) {
   const isInMonth = isDateInMonth(date.date, viewingDate);
   const isToday = isDateToday(date.date);
 
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+
+  if (!isDesktop && !isInMonth) {
+    return null;
+  }
+
   return (
     <div>
-      <div className="border-[0.5px] border-slate-200 bg-slate-200 text-center p-2 font-bold text-sm lg:hidden">
-        {dayKeysMap[date.day]}
+      <div className="border-[0.5px] border-slate-200 bg-slate-200 text-left lg:text-center p-2 font-bold text-sm lg:hidden">
+        {dayjs(date.dateString).format("DD/MM")}, {dayKeysMap[date.day]}
       </div>
-      <div className="border-[0.5px] p-2 flex flex-col h-full gap-2 border-slate-200 text-center text-sm">
-        <div>
+      <div className="border-[0.5px] p-2 flex flex-col h-full gap-2 border-slate-200 text-left lg:text-center text-sm">
+        <div className="hidden lg:inline-block">
           <span
             className={cn(
               "p-1 text-xs",
@@ -35,10 +44,14 @@ export default function Days({ date, viewingDate }: DaysProps) {
           </span>
         </div>
         {isInMonth && (
-          <div className="min-h-10 h-full flex flex-col gap-1">
+          <div className="lg:min-h-10 h-full flex flex-col gap-1">
             {date.events.map((event) => (
               <Event key={event.id} event={event} />
             ))}
+
+            {!isDesktop && date.events.length <= 0 ? (
+              <span className="text-slate-400">No events</span>
+            ) : null}
           </div>
         )}
       </div>
